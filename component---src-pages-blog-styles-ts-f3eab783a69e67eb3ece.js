23c3fe1486c22b166e2bc37f2572bcf0ffd919f3
chiyoku <chiyokuoni@gmail.com>
2021-05-29 12:53:55 -0300

    Updates

---
+++ component---src-pages-blog-styles-ts-f3eab783a69e67eb3ece.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[196],{2997:function(n,t,e){"use strict";e.r(t),e.d(t,{GlobalStyle:function(){return s},Wrapper:function(){return p}});var r,a,u=e(5761),d=e(9),s=(0,d.vJ)(r||(r=(0,u.Z)(["\n  body {\n    background: #EBEBEB;\n  }\n\n  * {\n    z-index: 1;\n  }\n"]))),p=d.ZP.div(a||(a=(0,u.Z)(["\n  padding-top: 80px;\n"])))}}]);
+//# sourceMappingURL=component---src-pages-blog-styles-ts-f3eab783a69e67eb3ece.js.map

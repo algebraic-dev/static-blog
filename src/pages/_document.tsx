4fff1e5b9351fdec5918817e1c6f318ca0a77da1
Felipe g <chiyokuoni@gmail.com>
2022-03-31 08:23:56 -0300

    fix: fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-
+        <link rel="preconnect" href="https://fonts.googleapis.com"/ >
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" /> 
       </Head>
       <body>
         <Main />
